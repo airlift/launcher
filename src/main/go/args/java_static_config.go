5ce6d841c38dae1399f009c9db51043957cfc663
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-04-25 13:43:55 +0200

    Do not warn on unsafe usage in JDK 24

--- src/main/go/args/java_static_config.go
+++ src/main/go/args/java_static_config.go
@@ -1,10 +1,10 @@
 package args
 
 const vectorApiIncubating = "--add-modules=jdk.incubator.vector"
+const allowUnsafeUsage = "--sun-misc-unsafe-memory-access=allow"
 
 var jvmSpecificConfig = map[string][]string{
 	"22": {vectorApiIncubating},
 	"23": {vectorApiIncubating},
-	// 24 is not released yet, but vector API will be still incubating
-	"24": {vectorApiIncubating},
+	"24": {vectorApiIncubating, allowUnsafeUsage},
 }

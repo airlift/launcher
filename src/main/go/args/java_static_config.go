34afa68c7c3147696d8a5ca6cb1c844934500ab2
kgniew <kamil.gniewek1@gmail.com>
2025-09-25 10:30:39 +0200

    Add --enable-native-access=ALL-UNNAMED for JDK 24 and 25

--- src/main/go/args/java_static_config.go
+++ src/main/go/args/java_static_config.go
@@ -2,10 +2,11 @@ package args
 
 const vectorApiIncubating = "--add-modules=jdk.incubator.vector"
 const allowUnsafeUsage = "--sun-misc-unsafe-memory-access=allow"
+const allowNativeUsage = "--enable-native-access=ALL-UNNAMED"
 
 var jvmSpecificConfig = map[string][]string{
 	"22": {vectorApiIncubating},
 	"23": {vectorApiIncubating},
-	"24": {vectorApiIncubating, allowUnsafeUsage},
-	"25": {vectorApiIncubating, allowUnsafeUsage},
+	"24": {vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"25": {vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
 }

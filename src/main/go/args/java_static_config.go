2b2b701a45c95276bc6d2c4720015ac59b903768
Mateusz "Serafin" Gajewski <github@wendigo.pl>
2025-10-15 17:02:11 +0200

    Disable RewriteBytecodes on arm64

--- src/main/go/args/java_static_config.go
+++ src/main/go/args/java_static_config.go
@@ -1,13 +1,24 @@
 package args
 
+import "runtime"
+
+var rewriteBytecodes = onArm64Only("-XX:-RewriteBytecodes") // https://bugs.openjdk.org/browse/JDK-8369506
 const vectorApiIncubating = "--add-modules=jdk.incubator.vector"
 const allowUnsafeUsage = "--sun-misc-unsafe-memory-access=allow"
 const allowNativeUsage = "--enable-native-access=ALL-UNNAMED"
 
 var jvmSpecificConfig = map[string][]string{
-	"22": {vectorApiIncubating},
-	"23": {vectorApiIncubating},
-	"24": {vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
-	"25": {vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
-	"26": {vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"21": {rewriteBytecodes},
+	"22": {rewriteBytecodes, vectorApiIncubating},
+	"23": {rewriteBytecodes, vectorApiIncubating},
+	"24": {rewriteBytecodes, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"25": {rewriteBytecodes, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+	"26": {rewriteBytecodes, vectorApiIncubating, allowUnsafeUsage, allowNativeUsage},
+}
+
+func onArm64Only(option string) string {
+	if runtime.GOARCH == "arm64" {
+		return option
+	}
+	return ""
 }
